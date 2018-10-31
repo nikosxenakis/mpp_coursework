008c3060c77c3f97fa7315ff1e9b433548b9dd21
Nikolaos Xenakis <nikosxenakis94@gmail.com>
2018-10-31 12:14:57 +0000

    dynamic allocation added

--- main.c
+++ main.c
@@ -21,8 +21,31 @@
 
 double boundaryval(int i, int m);
 
+void allocate_tables(double ***buf, double ***old, double ***new, double ***edge, int m, int n) {
+  size_t size_of_rows = (m+2)*sizeof(double*);
+  // size_t size_of_rows_buf = m*sizeof(double*);
+  size_t size_of_cols = (n+2)*sizeof(double);
+  // size_t size_of_cols_buf = n*sizeof(double);
+
+  // *buf = (double**)malloc(size_of_rows_buf);
+  *old = (double**)malloc(size_of_rows);
+  *new = (double**)malloc(size_of_rows);
+  *edge = (double**)malloc(size_of_rows);
+
+    for (int i = 0; i < m+2; ++i)
+    {
+      // if(i<m)
+      //   (*buf)[i] = (double*)malloc(size_of_cols_buf);
+      (*old)[i] = (double*)malloc(size_of_cols);
+      (*new)[i] = (double*)malloc(size_of_cols);
+      (*edge)[i] = (double*)malloc(size_of_cols);
+    }
+}
+
 int main (void) {
-  double old[M+2][N+2], new[M+2][N+2], edge[M+2][N+2];
+  // double old[M+2][N+2], new[M+2][N+2], edge[M+2][N+2];
+  double **old, **new, **edge;
+  // **buf, 
   double buf[M][N];
 
   int i, j, iter;
@@ -31,46 +54,52 @@ int main (void) {
 
   int world_rank, world_size;
   double start_time, end_time;
+  int m, n;
 
   MPI_Init(NULL, NULL);
 
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
+  printf("Process rank %d of %d processes\n", world_rank, world_size);
+  printf("Number of iterations = %d\n", MAXITER);
+
   MPI_Barrier(MPI_COMM_WORLD);
 
   if(world_rank == MASTER)
     start_time = MPI_Wtime();
 
-  printf("Process rank %d of %d processes\n", world_rank, world_size);
-  printf("Processing %d x %d image\n", M, N);
-  printf("Number of iterations = %d\n", MAXITER);
-
   filename = "resources/edgenew192x128.pgm";
 
+  pgmsize (filename, &m, &n);
+  printf("Processing %d x %d image\n", m, n);
+
+  allocate_tables(&buf, &old, &new, &edge, m, n);
+
+
   printf("\nReading <%s>\n", filename);
-  pgmread(filename, buf, M, N);
+  pgmread(filename, buf, m, n);
   printf("\n");
 
-  for (i=1;i<M+1;i++) {
-    for (j=1;j<N+1;j++) {
+  for (i=1;i<m+1;i++) {
+    for (j=1;j<n+1;j++) {
       edge[i][j]=buf[i-1][j-1];
     }
   }
 
-  for (i=0; i<M+2;i++) {
-    for (j=0;j<N+2;j++) {
+  for (i=0; i<m+2;i++) {
+    for (j=0;j<n+2;j++) {
       old[i][j]=255.0;
     }
   }
 
   /* Set fixed boundary conditions on the left and right sides */
-  for (j=1; j < N+1; j++) {
+  for (j=1; j < n+1; j++) {
     /* compute sawtooth value */
-    val = boundaryval(j, N);
+    val = boundaryval(j, n);
 
     old[0][j]   = (int)(255.0*(1.0-val));
-    old[M+1][j] = (int)(255.0*val);
+    old[m+1][j] = (int)(255.0*val);
   }
 
   for (iter=1;iter<=MAXITER; iter++) {
@@ -79,19 +108,19 @@ int main (void) {
     }
 
     /* Implement periodic boundary conditions on bottom and top sides */
-    for (i=1; i < M+1; i++) {
-      old[i][0]   = old[i][N];
-      old[i][N+1] = old[i][1];
+    for (i=1; i < m+1; i++) {
+      old[i][0]   = old[i][n];
+      old[i][n+1] = old[i][1];
     }
 
-    for (i=1;i<M+1;i++) {
-      for (j=1;j<N+1;j++) {
+    for (i=1;i<m+1;i++) {
+      for (j=1;j<n+1;j++) {
         new[i][j]=0.25*(old[i-1][j]+old[i+1][j]+old[i][j-1]+old[i][j+1] - edge[i][j]);
       }
     }
 
-    for (i=1;i<M+1;i++) {
-      for (j=1;j<N+1;j++) {
+    for (i=1;i<m+1;i++) {
+      for (j=1;j<n+1;j++) {
         old[i][j]=new[i][j];
       }
     }
@@ -99,14 +128,14 @@ int main (void) {
 
   printf("\nFinished %d iterations\n", iter-1);
 
-  for (i=1;i<M+1;i++) {
-    for (j=1;j<N+1;j++) {
+  for (i=1;i<m+1;i++) {
+    for (j=1;j<n+1;j++) {
       buf[i-1][j-1]=old[i][j];
     }
   }
 
   filename="output/imagenew192x128.pgm";
-  pgmwrite(filename, buf, M, N);
+  pgmwrite(filename, buf, m, n);
 
   MPI_Barrier(MPI_COMM_WORLD);
 
