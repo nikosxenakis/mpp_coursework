1a1a0eb99caed869c3f2c124d908d55baf7b3a7d
Nikolaos Xenakis <nikosxenakis94@gmail.com>
2018-11-12 23:38:41 +0000

    graphs updated

--- data_analyzer.py
+++ data_analyzer.py
@@ -21,40 +21,49 @@ def create_plots(title, x_axis_title, y_axis_title, labels, x_values, y_values, 
 
     plt.legend(loc=legent_pos)
 
-    fig.savefig('./output/' + str(title) + '.eps', format='eps', dpi=1000)
+    fig.savefig('./graphs/' + str(title) + '.eps', format='eps', dpi=1000)
 
-def analyze_data(data_file):
+def analyze_data(data_file, image):
 
     i = 0;
 
-    time = ()
-    pumas = ()
-    hares = ()
+    titles = ()
+    processes = ()
+    speedup = ()
+    time1 = 0
 
     for line in data_file:
         if i == 0:
             titles = line.split('\t')
         else:
             data = line.split('\t')
-            time = time + (float("%.3f" % float(data[0])),)
-            pumas = pumas + (float("%.3f" % float(data[1])),)
-            hares = hares + (float("%.3f" % float(data[2])),)
+            if data[0] == str("./resources/" + image):
+                if int(data[1]) == 1:
+                    time1 = int(data[1])
+                processes = processes + (float(data[1]),)
+                speedup = speedup + ( float("%.3f" % float(time1/float(data[2]))) ,)
         i = i + 1
 
     create_plots(
-        "average_density",
+        str(image + "_speedup"),
         titles[0],
-        "Average Density",
+        str(image + "_speedup"),
         [titles[1], titles[2]],
-        time,
-        [pumas, hares],
+        processes,
+        [speedup],
         0.4,
         "upper right"
     )
 
 
-path = './output/average_density.dat'
+path = './data/results.tsv'
 
 if os.path.exists(path):
     f = open(path, 'r')
-    analyze_data(f)
+    analyze_data(f, "edgenew192x128.pgm")
+    f = open(path, 'r')
+    analyze_data(f, "edgenew256x192.pgm")
+    f = open(path, 'r')
+    analyze_data(f, "edgenew512x384.pgm")
+    f = open(path, 'r')
+    analyze_data(f, "edgenew768x768.pgm")
